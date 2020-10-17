843917a710433c7f05d310dfa01876075ef8ddcf
bhupendra <bhupendra.sahu@laalsa.com>
2020-10-17 17:48:54 +0530

    added rect view

--- app/src/main/java/com/bhupi/a3dview/MainActivity.java
+++ app/src/main/java/com/bhupi/a3dview/MainActivity.java
@@ -2,23 +2,21 @@ package com.bhupi.a3dview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-TextView myview;
+TextView myview,myview1;
     RelativeLayout ll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myview=findViewById(R.id.myview);
+        myview1=findViewById(R.id.myview1);
         ll=findViewById(R.id.ll);
 
         myview.setOnTouchListener(new View.OnTouchListener() {
@@ -31,7 +29,7 @@ TextView myview;
                     {
                         myview.setElevation(25);
                         myview.setTranslationZ(0);
-                        myview.setBackgroundResource(R.drawable.pressed);
+                        myview.setBackgroundResource(R.drawable.circle_pressed);
                         myview.setTextColor(getResources().getColor(R.color.colorSkinAlpha));
                         return true;
                     }
@@ -39,8 +37,37 @@ TextView myview;
                     {
                         myview.setElevation(25);
                         myview.setTranslationZ(25);
-                        myview.setBackgroundResource(R.drawable.un_pressed);
+                        myview.setBackgroundResource(R.drawable.circle_un_pressed);
                         myview.setTextColor(getResources().getColor(R.color.colorSkin));
+
+                    }
+                }
+
+
+                return false;
+            }
+
+        });
+        myview1.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        myview1.setElevation(25);
+                        myview1.setTranslationZ(0);
+                        myview1.setBackgroundResource(R.drawable.rect_pressed);
+                        myview1.setTextColor(getResources().getColor(R.color.colorSkinAlpha));
+                        return true;
+                    }
+                    case MotionEvent.ACTION_UP:
+                    {
+                        myview1.setElevation(25);
+                        myview1.setTranslationZ(25);
+                        myview1.setBackgroundResource(R.drawable.rect_un_pressed);
+                        myview1.setTextColor(getResources().getColor(R.color.colorSkin));
 
                     }
                 }
